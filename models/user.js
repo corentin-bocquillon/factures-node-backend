cfd5252850b7f650674c91ed7cc7f83f79c6338f
Corentin Bocquillon <corentin@nybble.fr>
2020-02-01 15:22:29 +0100

    Add constraints to users table fields

--- models/user.js
+++ models/user.js
@@ -2,13 +2,35 @@
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('users', {
-        email: DataTypes.STRING,
-        hashedPassword: DataTypes.STRING,
-        salt: DataTypes.STRING,
-        companyName: DataTypes.STRING,
-        companyAddress: DataTypes.STRING,
-        companyNumber: DataTypes.STRING,
-        phoneNumber: DataTypes.STRING
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        hashedPassword: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        salt: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        companyName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        companyAddress: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        companyNumber: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     }, {});
     User.associate = function(models) {
         // associations can be defined here
