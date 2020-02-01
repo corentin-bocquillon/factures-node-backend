1969d2d71de2c2bb115dead31f5f929ab154d56c
Corentin Bocquillon <corentin@nybble.fr>
2020-02-01 19:45:02 +0100

    Bug fix

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,6 @@ const profile = require('../controllers/profile.js');
 module.exports = app => {
     app.route('/api/profile')
         .all(auth.isLoggedIn)
-        .get(profile.get())
-        .post(profile.post());
+        .get(profile.get)
+        .post(profile.post);
 };
